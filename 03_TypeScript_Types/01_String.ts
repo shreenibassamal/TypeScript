8ed6d0ea18c24b36c1f0005ca7bfe7069fb89e48
shreenibassamal <shreenibassamal500.com>
2025-04-22 06:27:13 +0530

    TypeScript Types

---
+++ 03_TypeScript_Types/01_String.ts
@@ -0,0 +1,5 @@
+let myName: string = "Shreenibas";
+let myAge: number = 32;
+let myMessage: string = `Hello ${myName}, you are ${myAge} years old.`; // Template string
+
+console.log(myMessage); // Output: Hello Shreenibas, you are 32 years old.ls
