a64fce4032deba874e2f7be9a0884f63568830e7
shreenibassamal <shreenibassamal500.com>
2025-04-22 06:17:41 +0530

    creation of Typescript file and compile it to javaScript file

---
+++ 01_basics/app.js
@@ -0,0 +1,3 @@
+console.log("Shreenibas");
+var message = "Hello World!";
+console.log(message);
---
+++ 01_basics/app.ts
@@ -0,0 +1,5 @@
+console.log("Shreenibas");
+
+let message: string = "Hello World!";
+console.log(message);   
+
